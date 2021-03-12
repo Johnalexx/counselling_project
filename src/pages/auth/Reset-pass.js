7fbd4aa7bc04c88916b0bf862ce909b33d955f0b
Johnalexx <adegbolajohn1@gmail.com>
2021-03-12 23:02:05 +0100

    Reset password

--- src/pages/auth/Reset-pass.js
+++ src/pages/auth/Reset-pass.js
@@ -4,6 +4,27 @@ function ResetPass() {
     return (
         <div>
             <h3>Reset Password</h3>
+            <div className="password-reset-form">
+                    <div className="login-card-body">
+                           <form>
+                               <div className="login-form-group">
+                                    <input class="form-control form-control-lg" id="username" type="text" placeholder="Username" autocomplete="off"/>
+                               </div>
+                               <div class="login-form-group">
+                                    <input class="form-control form-control-lg" id="password" type="password" placeholder="Password"/>
+                                </div>
+                                <div class="form-group">
+                                    <label class="custom-control custom-checkbox">
+                                        <input class="custom-control-input" type="checkbox"/>
+                                        <span class="custom-control-label">Remember Me</span>
+                                    </label>
+                                </div>
+                                <div class="login-form-submit">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+                                </div>
+                           </form>
+                       </div>
+            </div>
         </div>
     )
 }
