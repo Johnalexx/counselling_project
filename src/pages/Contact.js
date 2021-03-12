bd54a2b49893d4a2f1614af0f7ec233f6aa48c6c
Johnalexx <adegbolajohn1@gmail.com>
2021-03-12 14:52:40 +0100

    counselling system

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,9 @@ function Contact() {
     return (
         <div>
             <h3>Contact the Counsellor</h3>
+            <div className="contact-form">
+                <h4>Counsellor on Duty: Johnalex</h4>
+            </div>
         </div>
     )
 }
