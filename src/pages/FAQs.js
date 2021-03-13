5a7f23a6ed1e763ded1df80abf0127e751368cf5
Johnalexx <adegbolajohn1@gmail.com>
2021-03-13 19:54:48 +0100

    added faq

--- src/pages/FAQs.js
+++ src/pages/FAQs.js
@@ -3,7 +3,12 @@ import React from 'react'
 function FAQs() {
     return (
         <div>
-            <h3>What is Counselling?</h3>
+            <div className="faq-container">
+                <h3>What is Counselling?</h3>
+                <div className="counsellor-answers">
+                    <p>counseling is defined as, "a professional relationship that empowers diverse individuals, families, and groups to accomplish mental health, wellness, education, and career goals."</p>
+                </div>
+            </div>
         </div>
     )
 }
