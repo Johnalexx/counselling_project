944f0ae236664e3ce65a71c9ca2229fe1b147caf
Johnalexx <adegbolajohn1@gmail.com>
2021-03-13 19:59:24 +0100

    update faq

--- src/pages/FAQs.js
+++ src/pages/FAQs.js
@@ -4,9 +4,17 @@ function FAQs() {
     return (
         <div>
             <div className="faq-container">
-                <h3>What is Counselling?</h3>
-                <div className="counsellor-answers">
-                    <p>counseling is defined as, "a professional relationship that empowers diverse individuals, families, and groups to accomplish mental health, wellness, education, and career goals."</p>
+                <div className="single-faq-container">
+                    <h3>What is Counselling?</h3>
+                    <div className="counsellor-answers">
+                        <p>counseling is defined as, "a professional relationship that empowers diverse individuals, families, and groups to accomplish mental health, wellness, education, and career goals."</p>
+                    </div>
+                </div>
+                <div className="single-faq-container">
+                    <h3>Why is Counselling important?</h3>
+                    <div className="counsellor-answers">
+                        <p>Counseling provided by trained professionals can make a profound impact on the lives of individuals, families and communities. This service helps people navigate difficult life situations, such as the death of a loved one, divorce, natural disasters, school stress and the loss of a job.</p>
+                    </div>
                 </div>
             </div>
         </div>
