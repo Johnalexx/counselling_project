20729511c1abfc6f1c1647e76ed17f83dd98ace6
Johnalexx <adegbolajohn1@gmail.com>
2021-03-13 19:40:28 +0100

    update

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,7 +6,7 @@ function Index() {
         <div>
             <h3>Home Page</h3>
             <i>Johnalex Counsels</i>
-            <p></p>
+            <p><a href="https://github.com/johnalexx" target="_blank">View my GitHub page</a></p>
         </div>
     )
 }
