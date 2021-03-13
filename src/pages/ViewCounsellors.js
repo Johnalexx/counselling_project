aa969d4717c6c6d81fa9b758492e67150f8f2a93
Johnalexx <adegbolajohn1@gmail.com>
2021-03-13 15:31:48 +0100

    update

--- src/pages/ViewCounsellors.js
+++ src/pages/ViewCounsellors.js
@@ -1,23 +1,17 @@
 import React from 'react'
+import JT from '../images/js.png'
 
 function viewCounsellor(){
     return(
         <div>
             <h3>Available Counsellors are</h3>
-            <div className="counsellor-card-group">
-                <div className="counsellor-card">
-                    John Terry
-                </div>
-                <div>
-                    Ruben Dias
-                </div>
-                <div>
-                    Joao Cancelo
-                </div>
-                <div>
-                    Alejandro Grimaldo
-                </div>
-            </div>
+            <ul>
+                <li>Ruben Dias</li>
+                <li>John Stones</li>
+                <li>Ilkay Guendogan</li>
+                <li>Kevin DeBruyne</li>
+                <li>Joao Cancelo</li>
+            </ul>
         </div>
     )
 }
