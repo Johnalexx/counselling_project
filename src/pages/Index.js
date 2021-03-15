c0b5e273449bb1d05850be3295b0edbfe98257a7
Johnalexx <adegbolajohn1@gmail.com>
2021-03-15 12:09:51 +0100

    modify index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,8 +4,16 @@ import {Link} from 'react-router-dom';
 function Index() {
     return (
         <div>
-            <h3>Home Page</h3>
-            <i>Johnalex Counsels</i>
+            {/* Homepage JumboTron */}
+            <div className="index-jumbotron">
+                <h1 className="display-tron">Johnalex Counsels</h1>
+                <p className="hero-unit">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="marg-in"/>
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p class="le-ad">
+                        <a class="index-jumbotron-btn" href="#" role="button">Learn more</a>
+                    </p>
+            </div>
             <p><a href="https://github.com/johnalexx" target="_blank">View my GitHub page</a></p>
         </div>
     )
