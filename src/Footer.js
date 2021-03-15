7375f5434f0a99a451947f95ad5aa19783dc301b
Johnalexx <adegbolajohn1@gmail.com>
2021-03-15 11:50:54 +0100

    modified footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,7 @@ import React from 'react'
 function Footer(){
     return(
         <div className="footer">
-            <h1>Johnalex Footer</h1>
-            <i>Johnalex &copy; 2020</i>
+            <p><i>Johnalex &copy; 2020</i></p>
         </div>
     )
 }
