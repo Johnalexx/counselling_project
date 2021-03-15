3c8a1a7e5a7f1779aa71ba251f673f4476f95293
Johnalexx <adegbolajohn1@gmail.com>
2021-03-15 12:31:39 +0100

    update

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -7,11 +7,11 @@ function Index() {
             {/* Homepage JumboTron */}
             <div className="index-jumbotron">
                 <h1 className="display-tron">Johnalex Counsels</h1>
-                <p className="hero-unit">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p className="hero-unit">Improve Your Life, Reach Your Goals and Get Real Results.</p>
                 <hr class="marg-in"/>
                     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
                     <p class="le-ad">
-                        <a class="index-jumbotron-btn" href="#" role="button">Learn more</a>
+                        <a class="index-jumbotron-btn" href="/login" role="button">Get Started</a>
                     </p>
             </div>
             <p><a href="https://github.com/johnalexx" target="_blank">View my GitHub page</a></p>
