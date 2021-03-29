c39cd8bf59ca959c585673fbfd4b45d3db4fa54d
Johnalexx <adegbolajohn1@gmail.com>
2021-03-29 09:10:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -16,24 +16,29 @@ import './pages/Playground.css'
 import Login from './pages/auth/Login';
 import Signup from './pages/auth/Signup';
 import ResetPass from './pages/auth/Reset-pass';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Nav />
-        <Route path ="/" component={Index} exact/>
-        <Route path ="/contact" component={Contact} exact/>
-        <Route path ="/about" component={About} exact/>
-        <Route path ="/view-counsellors" component={viewCounsellor} exact/>
-        <Route path ="/faq" component={FAQs} exact/>
-        <Route path="/guide" component={Help} exact/>
-        <Route path="/play" component={Playground} exact/>
-        {/* Auth path */}
-        <Route path ="/login" component={Login} exact/>
-        <Route path ="/signup" component={Signup} exact/>
-        <Route path = "/reset-password" component={ResetPass} exact/>
-      </div>
+      <Switch>
+        <div className="App">
+          <Nav />
+          <Route path ="/" component={Index} exact/>
+          <Route path ="/contact" component={Contact} exact/>
+          <Route path ="/about" component={About} exact/>
+          <Route path ="/view-counsellors" component={viewCounsellor} exact/>
+          <Route path ="/faq" component={FAQs} exact/>
+          <Route path="/guide" component={Help} exact/>
+          <Route path="/play" component={Playground} exact/>
+          {/* Auth path */}
+          <Route path ="/login" component={Login} exact/>
+          <Route path ="/signup" component={Signup} exact/>
+          <Route path = "/reset-password" component={ResetPass} exact/>
+          {/* Error Pages
+          <Route path="*" component={NotFound} exact/> */}
+        </div>
+      </Switch>
       <Footer />
     </Router>
   );
